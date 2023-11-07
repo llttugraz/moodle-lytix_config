a8d8dd618e58ca7602dff71abfee5bf83b9f021b
Guenther Moser <moser@tugraz.at>
2023-11-07 11:34:36 +0100

    v1.0.8

--- version.php
+++ version.php
@@ -33,5 +33,5 @@ $plugin->dependencies = [
     'lytix_logs' => ANY_VERSION,
     'lytix_helper' => ANY_VERSION
 ];
-$plugin->release   = 'v1.0.7';
+$plugin->release   = 'v1.0.8';
 $plugin->supported = [401, 403];
