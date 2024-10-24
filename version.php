7ffb2217476d49fdfc4bdfb85ef0b0eed86adeb8
Gerhard Unger <gerhard.unger@tugraz.at>
2024-10-24 10:06:44 +0200

    [#173] Update release version to v1.0.9

--- version.php
+++ version.php
@@ -33,5 +33,5 @@ $plugin->dependencies = [
     'lytix_logs' => ANY_VERSION,
     'lytix_timeoverview' => ANY_VERSION,
 ];
-$plugin->release   = 'v1.0.8';
+$plugin->release   = 'v1.0.9';
 $plugin->supported = [401, 405];
