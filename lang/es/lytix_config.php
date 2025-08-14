707f26d06695d89212f1d5bb06b37ec90b4243a3
Kemal Halilovic <kemal.halilovic@tugraz.at>
2025-08-14 10:13:32 +0200

    translations

---
+++ lang/es/lytix_config.php
@@ -0,0 +1,28 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Language file es.
+ *
+ * @package lytix_config
+ * @author Moodle plugin: Language strings translator (local_translatelangstr) - automatically generated file
+ * @copyright 2025 Educational Technologies, Graz, University of Technology
+ * @license http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+$string['pluginname'] = 'Configuración de Lytix';
+$string['privacy:metadata'] = 'Este plugin no almacena ningún dato.';
+$string['privacy:nullproviderreason'] = 'Este complemento no tiene ninguna base de datos para almacenar la información de los usuarios. Solo utiliza las API de mod_assign para ayudar a mostrar la interfaz de calificación.';
---
+++ lang/fi/lytix_config.php
@@ -0,0 +1,28 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Language file fi.
+ *
+ * @package lytix_config
+ * @author Moodle plugin: Language strings translator (local_translatelangstr) - automatically generated file
+ * @copyright 2025 Educational Technologies, Graz, University of Technology
+ * @license http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+$string['pluginname'] = 'Lytix-asetukset';
+$string['privacy:metadata'] = 'Tämä laajennus ei tallenna mitään tietoja.';
+$string['privacy:nullproviderreason'] = 'Tällä laajennuksella ei ole tietokantaa käyttäjätietojen tallentamista varten. Se käyttää vain mod_assign-moduulin API-rajapintoja arviointirajapinnan näyttämiseen.';
